05b8c6218f83f15b131087f5b7e8db8b937e3f0a
Guangshi Jie <guangshi18@gmail.com>
2023-04-03 10:58:18 -0700

    add codes on routes/api

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,28 +1,78 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
+// // The `/api/categories` endpoint
+// // 1st way
+// router.get('/', (req, res) => {
+//   // find all categories
+//   Category.findAll({
+//     include: {
+//   // be sure to include its associated Products
+//       model: Product,
+//       attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+//     }
+//   })
+//     .then(dbCategoryData => {
+//       if(!dbCategoryData) {
+//         res.status(404).json({message: 'Not Found'});
+//         return;
+//       }
+//       res.json(dbCategoryData);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err)
+//     });
+// });
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
-});
+// // 2nd way
+// router.get('/', async (req, res) => {
+//   try {
+//     const categoryData = await Category.findAll();
+//     res.status(200).json(categoryData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- db/schema.sql
+++
@@ -1,3 +0,0 @@
-DROP DATABASE IF EXISTS commerce_db;
-
-CREATE DATABASE commerce_db; 
